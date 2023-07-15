e84bdd7112a36e5e9cf192f93c5eabde08a9d414
Rasel5267 <mahadiul09@gmail.com>
2023-07-16 05:27:23 +0600

    setup redux

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,14 @@
 import BookCard from '@/components/BookCard';
 import Hero from '@/components/Hero';
-import { useGetBooksQuery } from '@/redux/api/apiSlice';
-import { IBook } from '@/types/globalTypes';
+import Books from '@/data/BooksData';
 
 const Home = () => {
-  const { data, isLoading } = useGetBooksQuery(undefined);
-  const Books = data?.data?.slice(0, 10);
-
   return (
     <div className="pb-8">
       <Hero />
       <h1 className="text-5xl text-center mt-10 font-bold">Our Latest Books</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 px-5 mt-8">
-        {Books.map((book: IBook) => (
+        {Books.map((book) => (
           <BookCard key={book._id} {...book} />
         ))}
       </div>
