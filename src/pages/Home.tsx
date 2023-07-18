8d1f3d70c1616c37a0ff52d7977fec22676cf0c3
Rasel5267 <mahadiul09@gmail.com>
2023-07-18 12:57:46 +0600

    [done] confirmation alert for delete and show recent added book on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,21 @@ import { IBook } from '@/types/globalTypes';
 const Home = () => {
   const { data, isLoading } = useGetBooksQuery(undefined);
 
+  // Filter out any undefined items in the data array and create a shallow copy
+  const validData: IBook[] =
+    data?.data?.filter((item: IBook) => item.createdAt) || [];
+  const sortedBooks: IBook[] = [...validData];
+
+  // Sort the books based on createdAt in descending order
+  sortedBooks.sort((a, b) => {
+    const dateA = new Date(a.createdAt!).getTime(); // Here, we use 'a.createdAt!' to tell TypeScript that it's not undefined
+    const dateB = new Date(b.createdAt!).getTime(); // Here, we use 'b.createdAt!' to tell TypeScript that it's not undefined
+    return dateB - dateA;
+  });
+
+  // Get the first 10 recently added books
+  const Books: IBook[] = sortedBooks.slice(0, 10);
+
   return (
     <div className="pb-8">
       <Hero />
